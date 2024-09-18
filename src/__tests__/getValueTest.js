9f620ce401817b4012e17df74dff14e6a44e87f7
Chris Oloff <chris@uber5.com>
2024-09-18 15:26:33 +0200

    remove unnecessary symbol, lint formatting

--- src/__tests__/getValueTest.js
+++ src/__tests__/getValueTest.js
@@ -1,24 +1,20 @@
-const { addVictronInterfaces } = require('../index');
+const { addVictronInterfaces } = require("../index");
 
-describe('victron-dbus-virtual, GetValue being called on us', () => {
-
-  it('works for the happy case', async () => {
-
-    const declaration = { name: 'foo', properties: { IntProp: 'i' } };
+describe("victron-dbus-virtual, GetValue being called on us", () => {
+  it("works for the happy case", () => {
+    const declaration = { name: "foo", properties: { IntProp: "i" } };
     const definition = { IntProp: 42 };
     const bus = {
-      exportInterface: jest.fn()
+      exportInterface: jest.fn(),
     };
 
     addVictronInterfaces(bus, declaration, definition);
 
     expect(bus.exportInterface.mock.calls.length).toBe(2);
     expect(bus.exportInterface.mock.calls[1][0].GetValue).toBeDefined();
-    expect(bus.exportInterface.mock.calls[1][1]).toBe('/IntProp');
+    expect(bus.exportInterface.mock.calls[1][1]).toBe("/IntProp");
 
     const result = bus.exportInterface.mock.calls[1][0].GetValue();
-    expect(result).toEqual(['i', 42]);
-
+    expect(result).toEqual(["i", 42]);
   });
-
 });
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-
-const formatterSymbol = Symbol('formatter');
-
 function addVictronInterfaces(bus, declaration, definition) {
-
   const warnings = [];
 
   if (!declaration.name) {
-    throw new Error('Interface name is required');
+    throw new Error("Interface name is required");
   }
 
   if (!declaration.name.match(/^[a-zA-Z0-9_.]+$/)) {
     warnings.push(
-      `Interface name contains problematic characters, only a-zA-Z0-9_ allowed.`
+      `Interface name contains problematic characters, only a-zA-Z0-9_ allowed.`,
     );
   }
   if (!declaration.name.match(/^com.victronenergy/)) {
-    warnings.push('Interface name should start with com.victronenergy');
+    warnings.push("Interface name should start with com.victronenergy");
   }
 
   function wrapValue(t, v) {
     switch (t) {
-      case 'b':
-        return ['b', v];
-      case 's':
-        return ['s', v];
-      case 'i':
-        return ['i', v];
-      case 'd':
-        return ['d', v];
+      case "b":
+        return ["b", v];
+      case "s":
+        return ["s", v];
+      case "i":
+        return ["i", v];
+      case "d":
+        return ["d", v];
       default:
         return t.type ? wrapValue(t.type, v) : v;
     }
@@ -35,13 +31,13 @@ function addVictronInterfaces(bus, declaration, definition) {
 
   function unwrapValue([t, v]) {
     switch (t[0].type) {
-      case 'b':
+      case "b":
         return !!v[0];
-      case 's':
+      case "s":
         return v[0];
-      case 'i':
+      case "i":
         return Number(v[0]);
-      case 'd':
+      case "d":
         return Number(v[0]);
       default:
         throw new Error(`Unsupported value type: ${JSON.stringify(t)}`);
@@ -51,123 +47,123 @@ function addVictronInterfaces(bus, declaration, definition) {
   // we use this for GetItems and ItemsChanged
   function getProperties() {
     return Object.entries(declaration.properties || {}).map(([k, v]) => {
-      console.log('getProperties, entries, (k,v):', k, v);
+      console.log("getProperties, entries, (k,v):", k, v);
 
-      const format = (v.type && v.format) ? v.format : (v) => '' + v;
+      const format = v.type && v.format ? v.format : (v) => "" + v;
       return [
         k,
         [
-          ['Value', wrapValue(v, definition[k])],
-          ['Text', ['s', format(definition[k])]]
-        ]
+          ["Value", wrapValue(v, definition[k])],
+          ["Text", ["s", format(definition[k])]],
+        ],
       ];
     });
   }
 
   const iface = {
-    GetItems: function() {
+    GetItems: function () {
       return getProperties();
     },
-    emit: function() { }
+    emit: function () {},
   };
 
   const ifaceDesc = {
-    name: 'com.victronenergy.BusItem',
+    name: "com.victronenergy.BusItem",
     methods: {
-      GetItems: ['', 'a{sa{sv}}', [], []]
+      GetItems: ["", "a{sa{sv}}", [], []],
     },
     signals: {
-      ItemsChanged: ['a{sa{sv}}', '', [], []]
-    }
+      ItemsChanged: ["a{sa{sv}}", "", [], []],
+    },
   };
 
-  bus.exportInterface(iface, '/', ifaceDesc);
+  bus.exportInterface(iface, "/", ifaceDesc);
 
   // support GetValue and SetValue for each property
   for (const [k] of Object.entries(declaration.properties || {})) {
     bus.exportInterface(
       {
-        GetValue: function(/* value, msg */) {
+        GetValue: function (/* value, msg */) {
           const v = (declaration.properties || {})[k];
-          console.log('GetValue, definition[k] and v:', definition[k], v);
+          console.log("GetValue, definition[k] and v:", definition[k], v);
           return wrapValue(v, definition[k]);
         },
-        SetValue: function(value, /* msg */) {
+        SetValue: function (value /* msg */) {
           console.log(
-            'SetValue',
+            "SetValue",
             JSON.stringify(arguments[0]),
-            JSON.stringify(arguments[1])
+            JSON.stringify(arguments[1]),
           );
           try {
             definition[k] = unwrapValue(value);
-            iface.emit('ItemsChanged', getProperties());
+            iface.emit("ItemsChanged", getProperties());
             return 0;
           } catch (e) {
             console.error(e);
             return -1;
           }
-        }
+        },
       },
       `/${k}`,
       {
-        name: 'com.victronenergy.BusItem',
+        name: "com.victronenergy.BusItem",
         methods: {
-          GetValue: ['', 'v', [], ['value']],
-          SetValue: ['v', 'i', [], []]
-        }
-      }
+          GetValue: ["", "v", [], ["value"]],
+          SetValue: ["v", "i", [], []],
+        },
+      },
     );
   }
 
   async function addSettings(settings) {
     const body = [
-      settings.map(setting => [
-        ['path', wrapValue('s', setting.path)],
-        ['default', wrapValue('s', '' + setting.default)] // TODO: forcing value to be string
+      settings.map((setting) => [
+        ["path", wrapValue("s", setting.path)],
+        ["default", wrapValue("s", "" + setting.default)], // TODO: forcing value to be string
         // TODO: incomplete, min and max missing
-      ])
+      ]),
     ];
     return await new Promise((resolve, reject) => {
       bus.invoke(
         {
-          interface: 'com.victronenergy.Settings',
-          path: '/',
-          member: 'AddSettings',
-          destination: 'com.victronenergy.settings',
+          interface: "com.victronenergy.Settings",
+          path: "/",
+          member: "AddSettings",
+          destination: "com.victronenergy.settings",
           type: undefined,
-          signature: 'aa{sv}',
-          body: body
+          signature: "aa{sv}",
+          body: body,
         },
-        function(err, result) {
+        function (err, result) {
           if (err) {
             return reject(err);
           }
           return resolve(result);
-        }
+        },
       );
     });
   }
 
   async function removeSettings(settings) {
-    const body = [settings.map(setting => setting.path)];
+    const body = [settings.map((setting) => setting.path)];
 
     return new Promise((resolve, reject) => {
       bus.invoke(
         {
-          interface: 'com.victronenergy.Settings',
-          path: '/',
-          member: 'RemoveSettings',
-          destination: 'com.victronenergy.settings',
+          interface: "com.victronenergy.Settings",
+          path: "/",
+          member: "RemoveSettings",
+          destination: "com.victronenergy.settings",
           type: undefined,
-          signature: 'as',
-          body: body
+          signature: "as",
+          body: body,
         },
-        function(err, result) {
+        function (err, result) {
           if (err) {
             return reject(err);
           }
           return resolve(result);
-        }
+        },
       );
     });
   }
@@ -177,18 +173,18 @@ function addVictronInterfaces(bus, declaration, definition) {
       bus.invoke(
         {
           interface: interface_,
-          path: path || '/',
-          member: 'SetValue',
+          path: path || "/",
+          member: "SetValue",
           destination,
-          signature: 'v',
-          body: [wrapValue('s', '' + value)] // TODO: only supports string type for now
+          signature: "v",
+          body: [wrapValue("s", "" + value)], // TODO: only supports string type for now
         },
-        function(err, result) {
+        function (err, result) {
           if (err) {
             return reject(err);
           }
           resolve(result);
-        }
+        },
       );
     });
   }
@@ -198,29 +194,28 @@ function addVictronInterfaces(bus, declaration, definition) {
       bus.invoke(
         {
           interface: interface_,
-          path: path || '/',
-          member: 'GetValue',
-          destination
+          path: path || "/",
+          member: "GetValue",
+          destination,
         },
-        function(err, result) {
+        function (err, result) {
           if (err) {
             return reject(err);
           }
           resolve(result);
-        }
+        },
       );
     });
   }
 
   return {
-    emitItemsChanged: () => iface.emit('ItemsChanged', getProperties()),
+    emitItemsChanged: () => iface.emit("ItemsChanged", getProperties()),
     addSettings,
     removeSettings,
     setValue,
     getValue,
-    warnings
+    warnings,
   };
 }
 
-
-module.exports = { addVictronInterfaces, formatterSymbol };
+module.exports = { addVictronInterfaces };
